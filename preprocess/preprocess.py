c579cfe97fdbc22868625822bf340a5e0c9a6275
marcorossi5 <rssmrc.11@gmail.com>
2020-03-20 18:31:39 +0100

    Take care of memory

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -10,11 +10,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--source", "-s", default="*", type=str, help='Source .npy file name inside clear(noised)_files directory')
 parser.add_argument("--dir_name", "-d", default="../datasets", type=str, help='Directory path to datasets')
 
-def main(source, dir_name):
-    for i in ['clear_planes', 'clear_crops', 'noised_planes', 'noised_crops']:
-        if not os.path.isdir(os.path.join(dir_name,i)):
-            os.mkdir(os.path.join(dir_name,i))
-
+def get_planes_and_dump(source, dir_name):
     path_clear = os.path.join(dir_name,"clear_events", source)
     path_noise = os.path.join(dir_name,"noised_events", source)
     
@@ -39,7 +35,7 @@ def main(source, dir_name):
     np.save(os.path.join(dir_name,"noised_planes", 'val'), n[int(s*0.6):int(s*0.8)])
     np.save(os.path.join(dir_name,"noised_planes", 'test'), n[int(s*0.8):])
 
-    
+def crop_planes_and_dump(dir_name):
     for ss in ['train.npy', 'val.npy', 'test.npy']:
         clear_crops = []
         noised_crops = []
@@ -50,11 +46,25 @@ def main(source, dir_name):
             c = clear_plane[clear_plane!=np.inf].reshape([-1, clear_plane.shape[1]])
             n = noised_plane[noised_plane!=np.inf].reshape([-1, noised_plane.shape[1]])
 
-            for clear_crop, noised_crop in get_crop(c, n, 500, (32,32)):
+            for clear_crop, noised_crop in get_crop(c, n, 5, (32,32)):
                 clear_crops.append(clear_crop)
                 noised_crops.append(noised_crop)
         np.save(os.path.join(dir_name,"clear_crops", ss), np.stack(clear_crops))
         np.save(os.path.join(dir_name,"noised_crops", ss), np.stack(noised_crops))
+
+
+def main(source, dir_name):
+    for i in ['clear_planes', 'clear_crops', 'noised_planes', 'noised_crops']:
+        if not os.path.isdir(os.path.join(dir_name,i)):
+            os.mkdir(os.path.join(dir_name,i))
+
+    get_planes_and_dump(source, dir_name)
+    crop_planes_and_dump(dir_name)
+
+    for ss in ['train.npy', 'val.npy', 'test.npy']:
+        print(ss, np.load(os.path.join(dir_name, 'clear_crops', ss)).shape)
+        print(ss, np.load(os.path.join(dir_name, 'noised_crops', ss)).shape)
+
     
 
 if __name__ == '__main__':
