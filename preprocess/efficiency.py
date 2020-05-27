fix planes normalization

preprocess/efficiency.py
@@ -3,7 +3,7 @@ import argparse
 from time import time as tm
 import numpy as np
 import torch
-#from skimage.feature import canny
+from skimage.feature import canny
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--dir_name", "-p", default="../datasets",
@@ -15,20 +15,20 @@ def draw_results(a,b,c,d):
     print("------------------------------------------------")
     print("|{:>20}|{:>12}|{:>12}|".format("","Signal", "Background"))
     print("------------------------------------------------")
-    print("|{:>20}|{:>12.4f}|{:>12.4f}|".format("Predicted signal", a/tot,b/tot))
+    print("|{:>20}|{:>12.4e}|{:>12.4e}|".format("Predicted signal", a/tot,b/tot))
     print("------------------------------------------------")
-    print("|{:>20}|{:>12.4f}|{:>12.4f}|".format("Predicted background", c/tot,d/tot))
+    print("|{:>20}|{:>12.4e}|{:>12.4e}|".format("Predicted background", c/tot,d/tot))
     print("------------------------------------------------")
     print("{:>21}|{:>12}|{:>12}|".format("", "Sensitivity","Specificity"))
     print("                     ---------------------------")
-    print("{:>21}|{:>12.4f}|{:>12.4f}|".format("", a/(a+c), b/(b+d)))
+    print("{:>21}|{:>12.4e}|{:>12.4e}|".format("", a/(a+c), d/(b+d)))
     print("                     ---------------------------\n")
 
 def main(dir_name):
     
     for s in ['readout_', 'collection_']:
         for ss in ['train', 'val', 'test']:
-            clear = numpy.array(torch.load(os.path.join(dir_name,
+            clear = np.array(torch.load(os.path.join(dir_name,
                                                         'clear_planes',
                                                         "".join([s,ss]))))
 
@@ -37,6 +37,7 @@ def main(dir_name):
                 edges.append(canny(np.array(c)).astype(float))
 
             edges = np.stack(edges, 0)
+            clear[clear!=0] = 1
 
             d = clear*10-edges
 

preprocess/preprocessing_utils.py
@@ -16,10 +16,12 @@ ada_step = event_step // (2*readout_step + collection_step)
 def sample_binomial(num_trials, probs):
     return np.random.binomial(num_trials, probs)
 
-def normalize(img):
+def normalize(img, m=None):
     if img.max() == 0:
         return img
-    return (img-img.mean())/(img.max()-img.min())
+    if m is None:
+    	m = img.mean()
+    return (img-m)/(img.max()-img.min())
 
 def load_files(path_clear, path_noise):
     clear_files = glob.glob(path_clear)
@@ -62,7 +64,7 @@ def normalize_planes(clear_file, noised_file, r_idx, c_idx):
             print('skipped')
             continue
         r_n_clear.append(normalize(r_clear[i*readout_step:
-                                          (i+1)*readout_step]))
+                                          (i+1)*readout_step], 0))
         r_n_noised.append(normalize(r_noised[i*readout_step:
                                           (i+1)*readout_step]))
 
@@ -71,7 +73,7 @@ def normalize_planes(clear_file, noised_file, r_idx, c_idx):
             print('skipped')
             continue
         c_n_clear.append(normalize(c_clear[i*collection_step:
-                                          (i+1)*collection_step]))
+                                          (i+1)*collection_step], 0))
         c_n_noised.append(normalize(c_noised[i*collection_step:
                                             (i+1)*collection_step]))
 
@@ -92,8 +94,11 @@ def get_crop(clear_plane, n_crops=1000,
     c_x, c_y = crop_shape[0]//2, crop_shape[1]//2
 
     #does not work if clear_plane is a torch Tensor
-    clear_plane = np.array(clear_plane)
-    im = canny(clear_plane).astype(float)
+    im = torch.clone(clear_plane)
+    im[im!=0] = 1
+    #clear_plane = np.array(clear_plane)
+    #im = canny(clear_plane).astype(float)
+
     sgn = np.transpose(np.where(im==1))
     bkg = np.transpose(np.where(im==0))
 