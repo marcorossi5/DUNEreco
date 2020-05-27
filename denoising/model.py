7fef208078dc1ab0a19cd181236fa92fbbc1a97d
marcorossi5 <rssmrc.11@gmail.com>
2020-05-27 16:30:15 +0200

    CNNv2

--- denoising/model.py
+++ denoising/model.py
@@ -382,3 +382,154 @@ def get_GCNNv2(k, input_channels, hidden_channels,
     gcnnv2 = GCNNv2(k, input_channels, hidden_channels, patch_size)
 
     return gcnnv2
+
+def get_CNNv2(k, input_channels, hidden_channels,
+                    patch_size=(64, 64)):
+
+    class GraphConv(nn.Module):
+        def __init__(self, input_channels, out_channels, search_area=None):
+            super().__init__()
+            self.conv1 = nn.Conv2d(input_channels, out_channels, 1)
+            self.conv2 = nn.Conv2d(input_channels, out_channels, 3, padding=1)
+            self.conv3 = nn.Conv2d(input_channels, out_channels, 5, padding=2)
+
+        def forward(self, x):
+            return torch.mean(torch.stack([self.conv1(x),
+                                           self.conv2(x),
+                                           self.conv3(x)]), dim=0)
+
+    class PreProcessBlock(nn.Module):
+        def __init__(self, kernel_size, input_channels, out_channels):
+            super().__init__()
+            self.activ = nn.LeakyReLU(0.05)
+            self.convs = nn.Sequential(
+                nn.Conv2d(input_channels, out_channels, kernel_size,
+                                  padding=(kernel_size//2, kernel_size//2)),
+                self.activ,
+
+                nn.Conv2d(out_channels, out_channels, kernel_size,
+                                  padding=(kernel_size//2, kernel_size//2)),
+                self.activ,
+
+                nn.Conv2d(out_channels, out_channels, kernel_size,
+                                  padding=(kernel_size//2, kernel_size//2)),
+                self.activ,
+                )
+            self.bn = nn.BatchNorm2d(out_channels)
+
+            # out_channels -> out_channels
+            self.GC = GraphConv(out_channels, out_channels)
+
+        def forward(self, x):
+            x = self.convs(x)
+
+            x = self.activ(self.GC(x))
+            return x
+
+    class HPF(nn.Module):
+        """High Pass Filter"""
+        def __init__(self, input_channels, out_channels):
+            super().__init__()
+
+            self.conv = nn.Sequential(
+                nn.Conv2d(input_channels, input_channels, 3, padding=1),
+                nn.BatchNorm2d(input_channels),
+                nn.LeakyReLU(0.05))
+            
+            self.GC_1 = GraphConv(input_channels, input_channels)
+            self.GC_2 = GraphConv(input_channels, out_channels)
+            self.GC_3 = GraphConv(out_channels, out_channels)
+
+            self.act = nn.LeakyReLU(0.05)
+            
+        def forward(self, x):
+            x = self.conv(x)
+
+            x = self.act(self.GC_1(x))
+            x = self.act(self.GC_2(x))
+            return self.act(self.GC_3(x))
+
+
+    class LPF(nn.Module):
+        """Low Pass Filter"""
+        def __init__(self, input_channels, out_channels):
+            super().__init__()
+            self.conv = nn.Sequential(
+                nn.Conv2d(input_channels, input_channels, 3, padding=1),
+                nn.BatchNorm2d(input_channels),
+                nn.LeakyReLU(0.05))
+            
+            self.GC_1 = GraphConv(input_channels, input_channels)
+            self.bn_1 = nn.BatchNorm2d(input_channels)
+            self.GC_2 = GraphConv(input_channels, out_channels)
+            self.bn_2 = nn.BatchNorm2d(out_channels)
+            self.GC_3 = GraphConv(out_channels, out_channels)
+            self.bn_3 = nn.BatchNorm2d(out_channels)
+
+            self.act = nn.LeakyReLU(0.05)
+
+        def forward(self, x):
+            y = self.conv(x)
+
+            y = self.act(self.bn_1(self.GC_1(y)))
+            y = self.act(self.bn_2(self.GC_2(y)))
+            return x + self.act(self.bn_3(self.GC_3(y)))
+
+    loss_mse = nn.MSELoss()
+
+    class CNNv2(nn.Module):
+        def __init__(self, input_channels, hidden_channels,
+                    patch_size=(64, 64)):
+            super().__init__()
+            self.patch_size = patch_size
+            self.preprocessing_blocks = nn.ModuleList([
+                PreProcessBlock(3, input_channels, hidden_channels),
+                PreProcessBlock(5, input_channels, hidden_channels),
+                PreProcessBlock(7, input_channels, hidden_channels),
+            ])
+            self.LPF_1 = LPF(3*hidden_channels, 3*hidden_channels)
+            self.LPF_2 = LPF(3*hidden_channels, 3*hidden_channels)
+            self.LPF_3 = LPF(3*hidden_channels, 3*hidden_channels)
+            self.LPF_4 = LPF(3*hidden_channels, 3*hidden_channels)
+
+            self.HPF = HPF(3*hidden_channels, 3*hidden_channels)
+
+            self.GC = GraphConv(3*hidden_channels, input_channels)
+            
+            self.act = nn.Tanh()
+
+            self.a0 = nn.Parameter(torch.randn(1), requires_grad=True)
+            self.a1 = nn.Parameter(torch.randn(1), requires_grad=True)
+            self.a2 = nn.Parameter(torch.randn(1), requires_grad=True)
+            self.a3 = nn.Parameter(torch.randn(1), requires_grad=True)
+            self.a4 = nn.Parameter(torch.randn(1), requires_grad=True)
+            self.b0 = nn.Parameter(torch.randn(1), requires_grad=True)
+            self.b1 = nn.Parameter(torch.randn(1), requires_grad=True)
+            self.b2 = nn.Parameter(torch.randn(1), requires_grad=True)
+            self.b3 = nn.Parameter(torch.randn(1), requires_grad=True)
+            self.b4 = nn.Parameter(torch.randn(1), requires_grad=True)
+
+        def fit_image(self, x):
+            y = torch.cat([block(x) for block in
+                                        self.preprocessing_blocks], dim=1)
+            y_hpf = self.HPF(y)
+
+            y = self.LPF_1(y*(1-self.a0) + self.b0*y_hpf)
+            y = self.LPF_2(y*(1-self.a1) + self.b1*y_hpf)
+            y = self.LPF_3(y*(1-self.a2) + self.b2*y_hpf)
+            y = self.LPF_4(y*(1-self.a3) + self.b3*y_hpf)
+
+            graph = get_graph(y*(1-self.a4) + self.b4*y_hpf, self.k, l_mask)
+            return self.act(self.GC(y, graph) + x)
+
+        def forward(self, noised_image=None, clear_image=None):
+            if self.training:
+                out = self.fit_image(noised_image)
+                loss = loss_mse(out, clear_image)
+                return out, loss
+
+            return self.fit_image(noised_image)
+
+    cnnv2 = CNNv2(k, input_channels, hidden_channels, patch_size)
+
+    return cnnv2
