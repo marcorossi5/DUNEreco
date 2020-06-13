fix preprocessing (normalization, np)

denoising/dataloader.py
@@ -1,31 +1,10 @@
 import os
 import torch
+import numpy as np
 
 from model_utils import plot_crops
 from model_utils import plot_wires
 
-def minmax_norm(img):
-    """
-    MinMax normalization to be done on APAs or crops
-    Parameters:
-        img: torch.Tensor of shape (batch, w, h)
-    Output:
-    torch.Tensor of shape (batch, w, h)
-    """
-    return img
-    m = img.min(-1)[0]
-    m = m.min(-1)[0]
-
-    M = img.max(-1)[0]
-    M = M.max(-1)[0]
-
-    mask = M!=m
-
-    m = m.view(-1,1,1)
-    M = M.view(-1,1,1)
-
-    img[mask] = (img[mask]-m[mask])/(M[mask]-m[mask])
-    return img
 
 class CropLoader(torch.utils.data.Dataset):
     def __init__(self, args, name):
@@ -37,7 +16,7 @@ class CropLoader(torch.utils.data.Dataset):
                              'clear_crops/readout_%s_%d_%f'%(name,
                                                                 patch_size,
                                                                 p))
-        readout_clear = minmax_norm(torch.load(fname))
+        readout_clear = torch.Tensor(np.load(fname))
         if args.plot_dataset:
             sample = torch.randint(0,readout_clear.shape[0],(25,))
             wire = torch.randint(0,patch_size, (25,))
@@ -52,7 +31,7 @@ class CropLoader(torch.utils.data.Dataset):
                              'noised_crops/readout_%s_%d_%f'%(name,
                                                                 patch_size,
                                                                 p))
-        readout_noise = minmax_norm(torch.load(fname))
+        readout_noise = torch.Tensor(np.load(fname))
         if args.plot_dataset:
             plot_crops(args.dir_testing,
                        readout_noise,
@@ -66,7 +45,7 @@ class CropLoader(torch.utils.data.Dataset):
                              'clear_crops/collection_%s_%d_%f'%(name,
                                                                 patch_size,
                                                                 p))
-        collection_clear = minmax_norm(torch.load(fname))
+        collection_clear = torch.Tensor(np.load(fname))
         if args.plot_dataset:
             sample = torch.randint(0,collection_clear.shape[0],(25,))
             wire = torch.randint(0,patch_size, (25,))
@@ -83,7 +62,7 @@ class CropLoader(torch.utils.data.Dataset):
                              'noised_crops/collection_%s_%d_%f'%(name,
                                                                 patch_size,
                                                                 p))
-        collection_noise = minmax_norm(torch.load(fname))
+        collection_noise = torch.Tensor(np.load(fname))
         if args.plot_dataset:
             plot_crops(args.dir_testing,
                        collection_noise,
@@ -111,7 +90,7 @@ class PlaneLoader(torch.utils.data.Dataset):
         data_dir = args.dataset_dir
 
         fname = os.path.join(data_dir, 'clear_planes/%s'%file)
-        self.clear_planes = minmax_norm(torch.load(fname)).unsqueeze(1)
+        self.clear_planes = torch.Tensor(np.load(fname)).unsqueeze(1)
 
         if args.plot_dataset:
             sample = torch.randint(0,self.clear_planes.shape[0],(25,))
@@ -122,7 +101,7 @@ class PlaneLoader(torch.utils.data.Dataset):
                        "_".join([file, "clear"]),sample,wire)
 
         fname = os.path.join(data_dir, 'noised_planes/%s'%file)
-        self.noised_planes = minmax_norm(torch.load(fname)).unsqueeze(1)
+        self.noised_planes = torch.Tensor(np.load(fname)).unsqueeze(1)
         if args.plot_dataset:
             plot_wires(args.dir_testing,
                        self.noised_planes[:,0],

preprocess/preprocess.py
@@ -1,5 +1,5 @@
 import numpy as np
-import torch
+import np
 import os
 import sys
 import argparse
@@ -10,8 +10,6 @@ parser.add_argument("--source", "-s", default="*",
                     type=str, help='Source .npy file name')
 parser.add_argument("--dir_name", "-p", default="../datasets",
                     type=str, help='Directory path to datasets')
-parser.add_argument("--device", "-d", default="-2", type=str,
-                    help="-1 (automatic)/ -2 (cpu) / gpu number")
 parser.add_argument("--n_crops", "-n", default=500, type=int,
                     help="number of crops for each plane")
 parser.add_argument("--crop_edge", "-c", default=32, type=int,
@@ -52,19 +50,19 @@ def get_planes_and_dump(source, dir_name):
     c_r = clear_readout_planes[p]
     n_r = noised_readout_planes[p]
 
-    torch.save(torch.Tensor(c_r[:int(s*0.6)]),
-               os.path.join(dir_name,"clear_planes", 'readout_train'))
-    torch.save(torch.Tensor(c_r[int(s*0.6):int(s*0.8)]),
-               os.path.join(dir_name,"clear_planes", 'readout_val'))
-    torch.save(torch.Tensor(c_r[int(s*0.8):]),
-               os.path.join(dir_name,"clear_planes", 'readout_test'))
+    np.save(os.path.join(dir_name,"clear_planes", 'readout_train'),
+            c_r[:int(s*0.6)])
+    np.save(os.path.join(dir_name,"clear_planes", 'readout_val'),
+            c_r[int(s*0.6):int(s*0.8)])
+    np.save(os.path.join(dir_name,"clear_planes", 'readout_test'),
+            c_r[int(s*0.8):])
 
-    torch.save(torch.Tensor(n_r[:int(s*0.6)]),
-               os.path.join(dir_name,"noised_planes", 'readout_train'))
-    torch.save(torch.Tensor(n_r[int(s*0.6):int(s*0.8)]),
-               os.path.join(dir_name,"noised_planes", 'readout_val'))
-    torch.save(torch.Tensor(n_r[int(s*0.8):]),
-               os.path.join(dir_name,"noised_planes", 'readout_test'))
+    np.save(os.path.join(dir_name,"noised_planes", 'readout_train'),
+            n_r[:int(s*0.6)])
+    np.save(os.path.join(dir_name,"noised_planes", 'readout_val'),
+            n_r[int(s*0.6):int(s*0.8)])
+    np.save(os.path.join(dir_name,"noised_planes", 'readout_test'),
+            n_r[int(s*0.8):])
 
 
     s = len(clear_collection_planes)
@@ -72,29 +70,35 @@ def get_planes_and_dump(source, dir_name):
     c_c = clear_collection_planes[p]
     n_c = noised_collection_planes[p]
 
-    torch.save(torch.Tensor(c_c[:int(s*0.6)]),
-               os.path.join(dir_name,"clear_planes", 'collection_train'))
-    torch.save(torch.Tensor(c_c[int(s*0.6):int(s*0.8)]),
-               os.path.join(dir_name,"clear_planes", 'collection_val'))
-    torch.save(torch.Tensor(c_c[int(s*0.8):]),
-               os.path.join(dir_name,"clear_planes", 'collection_test'))
+    np.save(os.path.join(dir_name,"clear_planes", 'collection_train'),
+            c_c[:int(s*0.6)])
+    np.save(os.path.join(dir_name,"clear_planes", 'collection_val'),
+            c_c[int(s*0.6):int(s*0.8)])
+    np.save(os.path.join(dir_name,"clear_planes", 'collection_test'),
+            c_c[int(s*0.8):])
 
-    torch.save(torch.Tensor(n_c[:int(s*0.6)]),
-               os.path.join(dir_name,"noised_planes", 'collection_train'))
-    torch.save(torch.Tensor(n_c[int(s*0.6):int(s*0.8)]),
-               os.path.join(dir_name,"noised_planes", 'collection_val'))
-    torch.save(torch.Tensor(n_c[int(s*0.8):]),
-               os.path.join(dir_name,"noised_planes", 'collection_test'))
+    np.save(os.path.join(dir_name,"noised_planes", 'collection_train'),
+            n_c[:int(s*0.6)])
+    np.save(os.path.join(dir_name,"noised_planes", 'collection_val'),
+            n_c[int(s*0.6):int(s*0.8)])
+    np.save(os.path.join(dir_name,"noised_planes", 'collection_test'),
+            n_c[int(s*0.8):])
 
-def crop_planes_and_dump(dir_name, n_crops, device, crop_shape, p):
+def crop_planes_and_dump(dir_name, n_crops, crop_shape, p):
     for s in ['readout_', 'collection_']:
         for ss in ['train', 'val', 'test']:
-            clear_planes = torch.load(os.path.join(dir_name,
-                                                   "clear_planes",
-                                                   s+ss))
-            noised_planes = torch.load(os.path.join(dir_name,
-                                                    "noised_planes",
-                                                    s+ss))
+            clear_planes = np.load(os.path.join(dir_name,
+                                                 "clear_planes",
+                                                 s+ss))
+            noised_planes = np.load(os.path.join(dir_name,
+                                                 "noised_planes",
+                                                 s+ss))
+            clear_planes, clear_min,\
+            clear_max = putils.normalize(clear_planes)
+            
+            noised_planes, noised_min,\
+            noised_max = putils.normalize(noised_planes)
+            
             clear_crops = []
             noised_crops = []
             for clear_plane, noised_plane in zip(clear_planes,noised_planes):
@@ -102,27 +106,38 @@ def crop_planes_and_dump(dir_name, n_crops, device, crop_shape, p):
                 idx = putils.get_crop(clear_plane,
                                       n_crops = n_crops,
                                       crop_shape = crop_shape,
-                                      device = device,
                                       p = p)
 
                 clear_crops.append(clear_plane[idx])
                 noised_crops.append(noised_plane[idx])
 
-            clear_crops = torch.cat(clear_crops, 0)
-            noised_crops = torch.cat(noised_crops, 0)
+            clear_crops = np.cat(clear_crops, 0)
+            noised_crops = np.cat(noised_crops, 0)
                 
-            torch.save(clear_crops,
-                       os.path.join(dir_name,
-                       				"clear_crops",
-                       				"%s%s_%d_%f"%(s, ss, crop_shape[0], p)))
+            np.save(os.path.join(dir_name,
+                                 "clear_crops",
+                                 "%s%s_%d_%f"%(s, ss, crop_shape[0], p)),
+                    clear_crops)
 
             
-            torch.save(noised_crops,
-                       os.path.join(dir_name,
-                       				"noised_crops",
-                       				"%s%s_%d_%f"%(s, ss, crop_shape[0], p)))
+            np.save(os.path.join(dir_name,
+                                "noised_crops",
+                                "%s%s_%d_%f"%(s, ss, crop_shape[0], p)),
+                    noised_crops)
+
+            np.save(os.path.join(dir_name,
+                                "clear_crops",
+                                "postprocess_%s%s_%d_%f"%(s, ss, crop_shape[0], p)),
+                    np.array([clear_min, clear_max])
+                    )
+
+            np.save(os.path.join(dir_name,
+                                "noised_crops",
+                                "postprocess_%s%s_%d_%f"%(s, ss, crop_shape[0], p)),
+                    np.array([noised_min, noised_max])
+                    )
             
-def main(source, dir_name, device, n_crops, crop_edge, percentage):
+def main(source, dir_name, n_crops, crop_edge, percentage):
     crop_shape = (crop_edge, crop_edge)
     for i in ['clear_planes', 'clear_crops', 'noised_planes', 'noised_crops']:
         if not os.path.isdir(os.path.join(dir_name,i)):
@@ -132,44 +147,31 @@ def main(source, dir_name, device, n_crops, crop_edge, percentage):
     for s in ['readout_', 'collection_']:
         for ss in ['train', 'val', 'test']:
             print(s+ss + ' clear planes',
-                  torch.load(os.path.join(dir_name,
-                                          'clear_planes', s+ss)).shape)
+                  np.load(os.path.join(dir_name,
+                                       'clear_planes', s+ss)).shape)
             print(s+ss + ' noised planes',
-                  torch.load(os.path.join(dir_name,
-                                          'noised_planes', s+ss)).shape)
+                  np.load(os.path.join(dir_name,
+                                       'noised_planes', s+ss)).shape)
     
-    crop_planes_and_dump(dir_name, n_crops, device, crop_shape, percentage)
+    crop_planes_and_dump(dir_name, n_crops, crop_shape, percentage)
     for s in ['readout_', 'collection_']:
         for ss in ['train', 'val', 'test']:
             print(s+ss + ' clear crops',
-                  torch.load(os.path.join(dir_name,
-                                          'clear_crops',
-                                          "%s%s_%d_%f"%(s, ss,
-                                                        crop_shape[0],
-                                                        percentage))).shape)
+                  np.load(os.path.join(dir_name,
+                                       'clear_crops',
+                                       "%s%s_%d_%f"%(s, ss,
+                                                     crop_shape[0],
+                                                     percentage))).shape)
             print(s+ss + ' noised crops',
-                  torch.load(os.path.join(dir_name,
-                                          'noised_crops',
-                                          "%s%s_%d_%f"%(s, ss,
-                                                        crop_shape[0],
-                                                        percentage))).shape)
+                  np.load(os.path.join(dir_name,
+                                       'noised_crops',
+                                       "%s%s_%d_%f"%(s, ss,
+                                                     crop_shape[0],
+                                                     percentage))).shape)
 
 if __name__ == '__main__':
     args = vars(parser.parse_args())
-    dev = 0
 
-    if torch.cuda.is_available():
-        if int(args['device']) == -1:
-            gpu_num = get_freer_gpu()
-            dev = torch.device('cuda:{}'.format(gpu_num))
-        elif  int(args['device']) > -1:
-            dev = torch.device('cuda:{}'.format(args['device']))
-        else:
-            dev = torch.device('cpu')
-    else:
-        dev = torch.device('cpu')
-    args['device'] = dev
-    print('Working on device: {}\n'.format(args['device']))
     start = time.time()
     main(**args)
     print('Program done in %f'%(time.time()-start))

preprocess/preprocessing_utils.py
@@ -2,7 +2,6 @@ import os
 import glob
 
 import numpy as np
-import torch
 
 from skimage.feature import canny
 
@@ -11,13 +10,15 @@ collection_step = 960
 readout_step = 800
 time_len = 6000
 ada_step = event_step // (2*readout_step + collection_step)
-c_pedestal = 500
-r_pedestal = 1800
+#c_pedestal = 500
+#r_pedestal = 1800
 
-def normalize(img):
-    if img.max()==0:
-        return img
-    return (img-img.min())/(img.max()-img.min())
+def normalize(planes):
+    M = planes.max()
+    if M==0:
+        return planes, 0, M
+    m = planes.min()
+    return (planes - m)/(M-m), m, M
 
 def load_files(path_clear, path_noise):
     clear_files = glob.glob(path_clear)
@@ -42,7 +43,7 @@ def plane_idx():
 
     return readout, collection
 
-def normalize_planes(clear_file, noised_file, r_idx, c_idx):
+def stack_planes(clear_file, noised_file, r_idx, c_idx):
     r_clear = clear_file[r_idx]
     r_noised = noised_file[r_idx]
 
@@ -59,19 +60,19 @@ def normalize_planes(clear_file, noised_file, r_idx, c_idx):
         if r_clear[i*readout_step:(i+1)*readout_step].max() == 0:
             print('skipped')
             continue
-        r_n_clear.append(normalize(r_clear[i*readout_step:
-                                                  (i+1)*readout_step]-r_pedestal))
-        r_n_noised.append(normalize(r_noised[i*readout_step:
-                                                  (i+1)*readout_step]))
+        r_n_clear.append(r_clear[i*readout_step:
+                                                  (i+1)*readout_step])
+        r_n_noised.append(r_noised[i*readout_step:
+                                                  (i+1)*readout_step])
 
     for i in range(int(c_clear.shape[0]/collection_step)):
         if c_clear[i*collection_step:(i+1)*collection_step].max() == 0:
             print('skipped')
             continue
-        c_n_clear.append(normalize(c_clear[i*collection_step:
-                                                  (i+1)*collection_step]-c_pedestal))
-        c_n_noised.append(normalize(c_noised[i*collection_step:
-                                                    (i+1)*collection_step]))
+        c_n_clear.append(c_clear[i*collection_step:
+                                                  (i+1)*collection_step])
+        c_n_noised.append(c_noised[i*collection_step:
+                                                    (i+1)*collection_step])
 
     return np.stack(r_n_clear), np.stack(r_n_noised),\
            np.stack(c_n_clear), np.stack(c_n_noised)
@@ -82,17 +83,16 @@ def get_planes(clear_file, noised_file):
     """
     r_idx, c_idx = plane_idx()
 
-    return normalize_planes(clear_file, noised_file, r_idx, c_idx)
+    return stack_planes(clear_file, noised_file, r_idx, c_idx)
 
 def get_crop(clear_plane, n_crops=1000,
-            crop_shape=(32,32), device=torch.device('cpu'), p=0.5):
+            crop_shape=(32,32), p=0.5):
     x, y = clear_plane.shape
     c_x, c_y = crop_shape[0]//2, crop_shape[1]//2
 
-    #does not work if clear_plane is a torch Tensor
-    #im = torch.clone(clear_plane)
+    #im = np.copy(clear_plane)
     #im[im!=0] = 1
-    clear_plane = np.array(clear_plane)
+    clear_plane = np.copy(clear_plane)
     im = canny(clear_plane).astype(float)
 
     sgn = np.transpose(np.where(im==1))