fix normalization

denoising/dataloader.py
@@ -16,11 +16,12 @@ class CropLoader(torch.utils.data.Dataset):
 
         fname = os.path.join(data_dir,'train','crops',
                              f'{channel}_clear_{patch_size}_{p}.npy')
-        self.clear = torch.Tensor(np.load(fname))
+        clear = torch.Tensor(np.load(fname))
 
         fname = os.path.join(data_dir,'train','crops',
                              f'{channel}_noisy_{patch_size}_{p}.npy')
-        self.noisy = torch.Tensor(np.load(fname))
+        noisy = torch.Tensor(np.load(fname))
+
 
         if args.plot_dataset:
             sample = torch.randint(0,self.clear.shape[0],(25,))
@@ -37,6 +38,15 @@ class CropLoader(torch.utils.data.Dataset):
             plot_wires(args.dir_testing,
                        noisy,
                        "_".join([channel,'noisy',folder]),sample,wire)
+
+        #normalize crops
+        fname = os.path.join(args.dataset_dir,
+                             '_'.join([channel,'normalization.npy']))
+        m, M = np.load(fname)
+
+        self.clear = (clear-m)/(M-m)
+        self.noisy = (noisy-m)/(M-m)
+
     def __len__(self):
         return len(self.noisy)
     def __getitem__(self, index):
@@ -47,32 +57,29 @@ class PlaneLoader(torch.utils.data.Dataset):
         data_dir = os.path.join(args.dataset_dir, folder)
 
         fname = os.path.join(data_dir, 'planes', f'{channel}_clear.npy')
-        self.clear = torch.Tensor(np.load(fname))
+        clear = torch.Tensor(np.load(fname))
         
         fname = os.path.join(data_dir, 'planes', f'{channel}_noisy.npy')
-        self.noisy = torch.Tensor(np.load(fname))
+        noisy = torch.Tensor(np.load(fname))
         
         if args.plot_dataset:
-            sample = torch.randint(0,self.clear.shape[0],(25,))
-            wire = torch.randint(0,self.clear.shape[2], (25,))
+            sample = torch.randint(0,clear.shape[0],(25,))
+            wire = torch.randint(0,clear.shape[2], (25,))
             plot_wires(args.dir_testing,
-                       self.clear,
+                       clear,
                        "_".join([folder, file, "clear"]),sample,wire)
             plot_wires(args.dir_testing,
-                       self.noisy,
+                       noisy,
                        "_".join([folder, file, "noisy"]),sample,wire)
         
-        clear_M = self.clear.max()
-        clear_m = self.clear.min()
+        fname = os.path.join(args.dataset_dir,
+                             '_'.join([channel,'normalization.npy']))
+        self.norm = np.load(fname)
 
-        noisy_M = self.noisy.max()
-        noisy_m = self.noisy.min()
+        #clear planes don't need to be normalized
+        self.clear = self.clear
+        self.noisy = (noisy-self.norm[0])/(self.norm[1]-self.norm[0])
 
-
-        self.clear = (self.clear-clear_m)/(clear_M-clear_m)
-        self.noisy = (self.noisy-noisy_m)/(noisy_M-noisy_m)
-
-        self.norm = torch.stack([clear_M, clear_m, noisy_M, noisy_m])
 
     def __len__(self):
         return len(self.noisy)

denoising/train.py
@@ -54,8 +54,7 @@ def test_epoch(args, epoch, test_data, model):
             dn.append(answer)
         dn = torch.cat(dn)
         dn = recombine_img(dn, crops_shape, pad)
-        clear = clear * (norm[0]-norm[1]) + norm[1]
-        dn = dn * (norm[0]-norm[1]) + norm[1]
+        dn = dn * (norm[1]-norm[0]) + norm[0]
         loss.append(model.loss_fn(clear,dn).cpu().item())
         ssim.append(1-loss_ssim()(clear,dn).cpu().item())
         mse.append(torch.nn.MSELoss()(clear,dn).cpu().item())
@@ -119,8 +118,8 @@ def train(args, train_data, test_data, model):
 
         
     # initialize optimizer
-    optimizer=  optim.Adam(list(model.parameters()), lr=args.optim['lr'],
-                           amsgrad=args.optim['amsgrad'])
+    optimizer=  optim.Adam(list(model.parameters()), lr=args.lr,
+                           amsgrad=args.amsgrad)
     
     # start main loop
     time_all = np.zeros(args.epochs)

preprocess/preprocess.py
@@ -82,11 +82,6 @@ def crop_planes_and_dump(dir_name, n_crops, crop_shape, p):
         fname = os.path.join(dir_name,'planes',f'{s}_noisy.npy')
         noisy_planes = np.load(fname)[:,0]
 
-        clear_m = clear_planes.min()
-        clear_M = clear_planes.max()
-        noisy_m = noisy_planes.min()
-        noisy_M = noisy_planes.max()
-
         clear_crops = []
         noisy_crops = []
         for clear_plane, noisy_plane in zip(clear_planes,noisy_planes):
@@ -105,13 +100,11 @@ def crop_planes_and_dump(dir_name, n_crops, crop_shape, p):
 
         fname = os.path.join(dir_name,'crops',
                              f'{s}_clear_{crop_shape[0]}_{p}')
-        np.save(fname,
-                (clear_crops-clear_m)/(clear_M-clear_m))
+        np.save(fname, clear_crops)
 
         fname = os.path.join(dir_name,'crops',
                              f'{s}_noisy_{crop_shape[0]}_{p}')
-        np.save(fname,
-                (noisy_crops-noisy_m)/(noisy_M-noisy_m))
+        np.save(fname,noisy_crops)
             
 def main(dir_name, n_crops, crop_edge, percentage):
     crop_shape = (crop_edge, crop_edge)
@@ -123,6 +116,22 @@ def main(dir_name, n_crops, crop_edge, percentage):
         print(f'\n{s}:')
         dname = os.path.join(dir_name, s)
         get_planes_and_dump(dname)
+
+    #save the normalization
+    for s in ['readout', 'collection']:
+        fname = os.path.join(dir_name, 'train/planes', '_'.join([s,'clear.npy']))
+        c = np.load(fname)
+
+        fname = os.path.join(dir_name, 'train/planes', '_'.join([s,'noisy.npy']))
+        n = np.load(fname)
+
+        m = min(c.min(), n.min())
+        M = max(c.max(), n.max())
+
+        fname = os.path.join(dir_name,'_'.join([s,'normalization']))
+        np.save(fname,[m,M])
+
+
 
     dname = os.path.join(dir_name, 'train')
     crop_planes_and_dump(dname, n_crops, crop_shape, percentage)