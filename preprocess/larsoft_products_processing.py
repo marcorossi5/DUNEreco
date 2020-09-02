cff920408ddd46d09f3f1a6efbf9e90c5b5a1c9b
marcorossi5 <rssmrc.11@gmail.com>
2020-09-02 16:11:40 +0200

    add check figure for simch

--- preprocess/larsoft_products_processing.py
+++ preprocess/larsoft_products_processing.py
@@ -14,7 +14,7 @@ tdc_min = 0
 tdc_max = 6000
 channels = 2560*6
 
-def main(dir_name):
+def process_depo(dir_name):
     #f_wire = glob.glob(os.path.join(dirname, 'wire/*'))
     f_simch = glob.glob(os.path.join(dirname, 'simch/*'))
     for f_w,f_s in zip(f_wire, f_simch):
@@ -39,6 +39,39 @@ def main(dir_name):
         np.save(fname, ch_depo)
         fname = fname.replace('charge', 'energy')
         np.save(fname, en_depo)
+
+def main(dir_name):
+    process_depo(dirname)
+
+    #check if things were done right
+    f_raw = glob.glob(os.path.join(dirname, 'raw/raw*'))[0]
+    f_ch = glob.glob(os.path.join(dirname, 'simch/charge*'))[0]
+
+    raw = np.load(f_raw)[:,2:]
+    ch = np.load(f_ch)[:,2:]
+
+    plt.figure()
+    gs = fig.add_gridspec(nrows=2, ncols=3)
+    ax = plt.add_subplot(gs[0,:-1])
+    ax.imshow(ch[1600:2560])
+    ax.colorbar()
+    ax.set_title('Charge Deposition')
+
+    ax = plt.subplot(gs[1:,-1])
+    ax.imshow(raw[1600:2560])
+    ax.colorbar()
+    ax.set_title('Raw Digits')
+
+    ax = plt.subplot(gs[0,-1])
+    ax.plot(ch[2500], lw=0.2)
+    ax.set_title('Wire 2500, Energy')
+
+    ax = plt.subplot(gs[1,-1])
+    ax.plot(raw[2500], lw=0.2)
+    ax.set_title('Raw Wire 2500, Raw')
+
+    plt.savefig('../collection_t.png', dpi=300, bbox_inches='tight')
+    plt.close()
 
 
 if __name__ == '__main__':
