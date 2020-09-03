f27d6563d6d3c5a5e49c8d050d884589d2e1ceec
marcorossi5 <rssmrc.11@gmail.com>
2020-09-03 18:50:45 +0200

    fix

--- denoising/wiener_filter.py
+++ denoising/wiener_filter.py
@@ -27,7 +27,7 @@ def wiener_filter(img, kernel, K):
     kernel /= np.sum(kernel)
     dummy = np.copy(img)
     dummy = fft2(dummy)
-    kernel = fft2(kernel, s = img.shape)
+    kernel = fft2(kernel, s= img.shape)
     kernel = np.conj(kernel) / (np.abs(kernel) ** 2 + K)
     dummy = dummy * kernel
     dummy = np.abs(ifft2(dummy))
@@ -46,31 +46,35 @@ def rgb2gray(rgb):
 if __name__ == '__main__':
     # Load image and convert it to gray scale
     file_name = os.path.join("../datasets/denoising/val", 'planes', 'collection_clear.npy') 
-    img = np.load(file_name)[0]
+    img = np.load(file_name)[0,0]
 
     # Blur the image
-    file_name = os.path.join("../datasets/denoising/val", 'planes', 'collection_clear.npy') 
-    noisy_img = np.load(file_name)[0]
-
-        fname = "../datasets/denoising/collection_normalization.npy"
-        norm = np.load(fname)
+    file_name = os.path.join("../datasets/denoising/val", 'planes', 'collection_noisy.npy') 
+    noisy_img = np.load(file_name)[0,0]
 
     # Apply Wiener Filter
     kernel = gaussian_kernel(3)
     filtered_img = wiener_filter(noisy_img, kernel, K = 10)
 
-        fig = plt.figure()
-        gs = fig.add_gridspec(nrows=2, ncols=1, wspace=1.5)
+    fig = plt.figure()
+    gs = fig.add_gridspec(nrows=2, ncols=1, wspace=3)
 
-        ax = fig.add_subplot(gs[0])
-        z = ax.imshow(img[0])
-        plt.colorbar(z, ax=ax)
-        ax.set_title('Labels')
+    ax = fig.add_subplot(gs[0])
+    ax.plot(img[10], lw=0.3)
+    #z = ax.imshow(img)
+    #plt.colorbar(z, ax=ax)
+    ax.set_title('Labels')
 
-        ax = fig.add_subplot(gs[1])
-        z = ax.imshow(filtered_img[0])
-        plt.colorbar(z, ax=ax)
-        ax.set_title('Predicted')
+    ax = fig.add_subplot(gs[1])
+    ax.plot(filtered_img[10], lw=0.3)
+    #z = ax.imshow(filtered_img)
+    #plt.colorbar(z, ax=ax)
+    ax.set_title('Predicted')
         
     plt.savefig('wiener_filter.png', dpi=300, bbox_inches='tight')
-        plt.close()
+    plt.close()
+
+    import torch
+    loss = torch.nn.MSELoss()
+    mse = loss(torch.Tensor(filtered_img-500), torch.Tensor(img))
+    print('MSE clear-wiener filtered: ', mse)
