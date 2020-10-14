e9fe5295a564aa535354112d87a011be99abdbc9
marcorossi5 <rssmrc.11@gmail.com>
2020-10-14 08:23:54 +0200

    add pandora products process and analysis

---
+++ denoising/benchmarks/pandora_hits_results.py
@@ -0,0 +1,88 @@
+"""
+This module computes recob::hits results for planes in the test set
+
+Threshold is the cut below which a clear raw digit is considered an
+hit with signal.
+
+Output file contains accuracy, sensitivity and specificity provided
+with mean values and uncertainties.
+"""
+from analysis.analysis_roi import confusion_matrix
+import os
+import sys
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+import time as tm
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+PARSER = argparse.ArgumentParser()
+parser.add_argument("--dirname", "-p", default="../datasets/backup/test",
+                    type=str, help='Directory path to datasets')
+PARSER.add_argument("--threshold", "-t", default=3.5, type=float,
+                    help="Threshold to distinguish signal/noise in labels")
+
+
+def main(dirname, threshold):
+    # Load true hits
+    # TODO: must subtract pedestal!
+    file_name = os.path.join(dirname, 'planes', 'collection_clear.npy')
+    true_hits = np.load(file_name)
+    mask = np.logical_and(true_hits >= 0, true_hits <= threshold)
+    true_hits[mask] = 0
+    true_hits[~mask] = 1
+
+    sig = np.count_nonzero(true_hits)
+    tot = true_hits.size
+
+    print('Percentage of pixels with signal:', sig/tot)
+    true_hits = true_hits.astype(bool)
+
+    # TODO: calculate acc, sns, spc (mean +- inc)
+
+    # Load recob::hits
+    file_name = os.path.join(dirname, "benchmarks/hits",
+                             "collection_hits.npy")
+
+    pandora_hits = np.load(file_name)
+
+    acc = []
+    sns = []
+    spc = []
+
+    for pandora_hit, true_hit in zip(pandora_hits, true_hits):
+        hit = pandora_hit[true_hit]
+        no_hit = pandora_hit[~true_hit]
+
+        tp, fp, fn, tn = confusion_matrix(hit, no_hit)
+        acc.append((tp+tn)/(tp+fp+fn+tn))
+        sns.append(tp/(fn+tp))
+        spc.append(tn/(fp+tn))
+
+    acc_mean = np.mean(acc)
+    acc_std = np.std(acc) / np.sqrt(len(acc))
+
+    sns_mean = np.mean(sns)
+    sns_std = np.std(sns) / np.sqrt(len(sns))
+
+    spc_mean = np.mean(spc)
+    spc_std = np.std(spc) / np.sqrt(len(spc))
+
+    res = np.array([[acc_mean, acc_std],
+                    [sns_mean, sns_std],
+                    [spc_mean, spc_std]])
+
+    print(res)
+
+    dir_name = 'denoising/benchmarks/results/'
+    fname = dir_name + 'pandora_hits_metrics'
+    np.save(fname, res)
+
+
+if __name__ == '__main__':
+    args = vars(PARSER.parse_args())
+    START = tm.time()
+    main(**args)
+    print('Program done in %f' % (tm.time()-START))
--- preprocess/preprocess_recob.py
+++ preprocess/preprocess_recob.py
@@ -16,6 +16,7 @@ N_COLLECTION = 960
 N_APAS = 6
 N_TICKS = 6000
 
+
 def process_hits(fname):
     hits = np.load(os.path.join(fname))
 
@@ -24,12 +25,13 @@ def process_hits(fname):
     for apa in range(N_APAS):
         first_ch = N_CHANNELS*apa + 2*N_INDUCTION
         last_ch = N_CHANNELS*(apa+1)
-        mask = np.logical_and(hits[:,0] >= first_ch, hits[:,0] < last_ch)
+        mask = np.logical_and(hits[:, 0] >= first_ch, hits[:, 0] < last_ch)
 
         for hit in hits[mask]:
             ch = hit[0] - first_ch
             ROIs[apa, ch, hit[1]:hit[2]] = 1
     return ROIs
+
 
 def process_wires(fname):
     wires = np.load(os.path.join(fname))
@@ -41,6 +43,7 @@ def process_wires(fname):
         coll_wires.append(wires[first_ch:last_ch, 2:])
     return coll_wires
 
+
 def process_hits_and_dump(dirname):
     """
     Processes hits to cast them into an array of shape (N_CHANNELS, N_TICKS).
@@ -51,13 +54,13 @@ def process_hits_and_dump(dirname):
     input_dir = os.path.join(dirname, "pandora_out")
     output_dir = os.path.join(dirname, "hits")
     fnames = glob.glob(os.path.join(input_dir, "*recobhits*"))
-    
+
     ROIs = []
     for fname in fnames:
         ROIs.append(process_hits(fname))
-    
+
     ROIs = np.concatenate(ROIs)
-    outname = os.path.join(output_dir, "collection_hits")
+    outname = os.path.join(output_dir, "pandora_collection_hits")
     np.save(outname, ROIs)
 
 
@@ -72,12 +75,12 @@ def process_wires_and_dump(dirname):
     input_dir = os.path.join(dirname, "pandora_out")
     output_dir = os.path.join(dirname, "wires")
     fnames = glob.glob(os.path.join(input_dir, "*recobwires*"))
-    
+
     wires = []
     for fname in fnames:
         wires.extend(process_wires(fname))
     wires = np.stack(wires)
-    outname = os.path.join(output_dir, "collection_wires")
+    outname = os.path.join(output_dir, "pandora_collection_wires")
     np.save(outname, wires)
 
 
@@ -91,4 +94,4 @@ if __name__ == '__main__':
     args = vars(parser.parse_args())
     START = tm.time()
     main(**args)
-    print('Program done in %f'%(tm.time()-START))
+    print('Program done in %f' % (tm.time()-START))
