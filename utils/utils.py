4502b9ce652858789cb48173e70f054bdcdabde5
marcorossi5 <rssmrc.11@gmail.com>
2020-11-17 19:58:08 +0100

    fix compute psnr

--- utils/utils.py
+++ utils/utils.py
@@ -35,7 +35,7 @@ def compute_psnr(image, noisy, reduction='mean'):
         x2 = noisy.reshape(nimages, -1)
         mse = torch.nn.MSELoss(reduction='none')(x1,x2).data.mean(-1)
         m2 = x1.max(-1).values**2
-        psnr = torch.where(m2 == 0, torch.Tensor([0.]), torch.log10(m2/mse))
+        psnr = torch.where(m2 == 0, torch.Tensor([0.]), 10*torch.log10(m2/mse))
         if reduction == 'none':
             return psnr
         elif reduction == 'mean':
