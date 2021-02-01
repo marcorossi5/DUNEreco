347b4d605802bb87739039f66864c322ec5a7179
marcorossi5 <rssmrc.11@gmail.com>
2021-02-01 14:53:45 +0100

    fix

--- denoising/wires_stats.py
+++ denoising/wires_stats.py
@@ -1,12 +1,16 @@
 import numpy as np
 import argparse
 from time import time
-from hitreco import evt2planes
+from hitreco import evt2planes, planes2evt
+import torch
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--fname", "-f",  type=str, help="Event root folder",
                    default="../datasets/20201124/test/evts/p2GeV_cosmics_wire_ev9.npy")
+parser.add_argument("--fit", action='store_true',
+                    help="Use PyROOT to fit the normalization constant")
 
+dev = "cuda:0"
 
 def fit_constant(wires, clear):
     from ROOT import TGraph, TF1
@@ -32,25 +36,44 @@ def compute_constants(wires, labels):
     return k
 
 
-def main(fname):
-    wires = np.load(fname)
-    _, cwires = evt2planes(wires)
+def main(fname, fit):
+    wires = np.load(fname)[:,2:]
+    iwires, cwires = evt2planes(wires)
 
     fname = fname.replace("wire", "rawdigit_noiseoff")
-    labels = np.load(fname)
-    _, clabels = evt2planes(labels)
+    labels = np.load(fname)[:,2:]
+    ilabels, clabels = evt2planes(labels)
 
-    # k = compute_constants(cwires, clabels)
-    k = np.array([[3.55240744, 0.09703621],
-                  [1.75292430, 0.02975895],
-                  [1.94628324, 0.03519356],
-                  [1.68464956, 0.02822732],
-                  [1.36381451, 0.02297976],
-                  [1.69213512, 0.02634354]])
-    
+    if fit:
+        ik = compute_constants(iwires, ilabels)
+        ck = compute_constants(cwires, clabels)
+    else:
+        ik = np.array([[6.44518276e-01, 6.31853618e-01],
+                       [1.24100913e+00, 1.59996499e-02],
+                       [1.01106692e+00, 1.58423364e-02],
+                       [1.03538924e+00, 6.87743815e-03],
+                       [7.61434061e-01, 1.39250640e-02],
+                       [9.94644122e-01, 5.43702747e-03],
+                       [7.39669829e-01, 8.73465559e-03],
+                       [9.87428925e-01, 5.24721814e-03],
+                       [1.01346316e+00, 6.88380304e-03],
+                       [1.02172429e+00, 1.78829187e-03],
+                       [1.02850125e+00, 5.42467795e-03],
+                       [1.02023728e+00, 1.01015753e-03]]
+                     )
+        ck = np.array([[3.35929051, 0.09637187],
+                       [2.07739054, 0.04431658],
+                       [1.74806175, 0.02859704],
+                       [1.83722203, 0.03261106],
+                       [1.37893158, 0.02219072],
+                       [1.32170305, 0.01274681]]
+                      )
+    inormalized = iwires*ik[:,:1,None,None]
+    cnormalized = cwires*ck[:,:1,None,None]
+    normalized = planes2evt(inormalized, cnormalized)
+
     from hitreco import compute_metrics
-    compute_metrics(cwires*k[:,0], clabels, 'dn')
-
+    compute_metrics(normalized, labels, 'dn')
 
 
 if __name__ == "__main__":
