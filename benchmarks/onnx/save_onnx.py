c210a05396b9d40052ffc45c0cee165bb218800d
marcorossi5 <rssmrc.11@gmail.com>
2022-02-11 11:07:59 +0100

    various fixings and formatting

--- benchmarks/onnx/save_onnx.py
+++ benchmarks/onnx/save_onnx.py
@@ -1,4 +1,3 @@
-
 """
     This module exports the query denoising network to `onnx` format.
 
@@ -8,7 +7,7 @@
     python benchmarks/onnx/save_onnx.py <modeltype> --onnx <path>
     ```
 
-    Note: the current implementation works only for modeltype `cnn`
+    Note: the current implementation works only for modeltype cnn | gcnn 
 
     The onnx export has a fixed input shape. Change the value of the 
     batch_size with `--batch_size` flag according to your needs, default is 32.
@@ -17,6 +16,7 @@ from pathlib import Path
 import argparse
 import torch
 from dunedn.networks.models import USCG_Net, GCNN_Net
+
 
 def get_dummy_input_and_uscg():
     """
@@ -28,8 +28,8 @@ def get_dummy_input_and_uscg():
     # USCG Net
     h = 100
     w = 500
-    pixels = h*w
-    dummy_input = torch.arange(pixels).reshape([1, 1, h, w])/ pixels
+    pixels = h * w
+    dummy_input = torch.arange(pixels).reshape([1, 1, h, w]) / pixels
     return dummy_input, USCG_Net(h=h, w=w)
 
 
@@ -39,7 +39,7 @@ def get_dummy_input_and_gcnn(modeltype, batch_size):
     ----------
         - modeltype: str, available options cnn | gcnn
         - batch_size: int, the dummy dataset batch size
-    
+
     Returns
     -------
         - torch.Tensor, the dummy input dataset of shape=(batch_size, 1, crop_edge, crop_edge)
@@ -48,9 +48,13 @@ def get_dummy_input_and_gcnn(modeltype, batch_size):
     # GCNN Net
     crop_edge = 32
     # edge_size = (edge_h, edge_w)
-    pixels  = batch_size * crop_edge ** 2
-    dummy_input =  torch.arange(pixels).reshape([batch_size, 1, crop_edge, crop_edge]) / pixels
-    return dummy_input, GCNN_Net(modeltype, "dn", crop_edge, 1, 32)
+    pixels = batch_size * crop_edge**2
+    dummy_input = (
+        torch.arange(pixels).reshape([batch_size, 1, crop_edge, crop_edge]) / pixels
+    )
+    k = 8 if modeltype == "gcnn" else None
+    return dummy_input, GCNN_Net(modeltype, "dn", crop_edge, 1, 32, k=k)
+
 
 def get_dummy_input_and_model(modeltype, batch_size):
     """
@@ -58,7 +62,7 @@ def get_dummy_input_and_model(modeltype, batch_size):
     ----------
         - modeltype: str, available options uscg | cnn | gcnn
         - batch_size: int, the dummy dataset batch size (only for cnn | gcnn)
-    
+
     Returns
     -------
         - torch.Tensor, dummy data for forward pass
@@ -78,13 +82,24 @@ def get_dummy_input_and_model(modeltype, batch_size):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("modeltype", help="model type, available options: uscg | cnn | gcnn")
-    parser.add_argument("--onnx", type=Path, help="the output onnx model filename", default="network.onnx")
-    parser.add_argument("--batch_size", type=int, help="onnx model batch size", default=32)
+    parser.add_argument(
+        "modeltype", help="model type, available options: uscg | cnn | gcnn"
+    )
+    parser.add_argument(
+        "--onnx",
+        type=Path,
+        help="the output onnx model filename",
+        default="network.onnx",
+    )
+    parser.add_argument(
+        "--batch_size", type=int, help="onnx model batch size", default=32
+    )
     args = parser.parse_args()
 
     if args.modeltype == "uscg" and args.batch_size != 1:
-        print(f"WARNING: uscg network accepts batch size equal to 1 only: found {args.batch_size}, skipping this")
+        print(
+            f"WARNING: uscg network accepts batch size equal to 1 only: found {args.batch_size}, skipping this"
+        )
     dummy_input, model = get_dummy_input_and_model(args.modeltype, args.batch_size)
 
     input_names = ["input"]
@@ -93,7 +108,7 @@ if __name__ == "__main__":
     torch.onnx.export(
         model,
         dummy_input,
-        args.output,
+        args.onnx,
         verbose=True,
         input_names=input_names,
         output_names=output_names,
