6342902f4cdb849376229bdeac1718b72e0df10a
marcorossi5 <rssmrc.11@gmail.com>
2022-06-06 12:45:18 +0200

    remove sphinx static paths

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,6 +4,15 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath(f"../../src"))
 
 # -- Project information -----------------------------------------------------
 
@@ -13,16 +22,6 @@ author = 'Marco Rossi'
 
 # The full version, including alpha/beta/rc tags
 release = '2.0.0'
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-import sys
-sys.path.insert(0, os.path.abspath(f"../../src/")) # {project}"))
 
 # -- General configuration ---------------------------------------------------
 
@@ -54,6 +53,6 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 html_show_sourcelink = True
