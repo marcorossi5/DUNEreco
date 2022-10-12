108faefa0d11f3fbbc1746aec7700ec473c276d1
marcorossi5 <rssmrc.11@gmail.com>
2022-10-13 00:14:50 +0200

    fix pytest crash

--- src/dunedn/tests/test_image_tiling.py
+++ src/dunedn/tests/test_image_tiling.py
@@ -7,7 +7,7 @@ from dunedn.geometry.helpers import evt2planes, planes2evt
 from dunedn.geometry.pdune import geometry
 
 
-def test_event_to_planes(events):
+def run_event_to_planes(events):
     iplanes, cplanes = evt2planes(events)
     assert len(iplanes.shape) == 4, f"got iplanes rank: {len(iplanes.shape)}"
     assert len(cplanes.shape) == 4, f"got cplanes rank: {len(cplanes.shape)}"
@@ -25,7 +25,7 @@ def test_event_to_planes(events):
     np.testing.assert_equal(events, reco_events)
 
 
-def test_crop_converter(events):
+def run_crop_converter(events):
     # test converter that exactly divide the event width
     converter1 = Converter((80, 1000))
     splits = converter1.image2crops(events)
@@ -43,7 +43,11 @@ def test_crop_converter(events):
     np.testing.assert_equal(events, reco_events)
 
 
-if __name__ == "__main__":
+def test_image_tiling():
     events = np.arange(2 * 15360 * 6000).reshape(2, 1, 15360, 6000) * 0.1
-    test_event_to_planes(events)
-    test_crop_converter(events)
+    run_event_to_planes(events)
+    run_crop_converter(events)
+
+
+if __name__ == "__main__":
+    test_image_tiling()
